c671f072fe6214a1f7666b212a9ede6e51a40e05
Anna Garmashevskaya <anniegemail@gmail.com>
2016-08-05 11:24:53 +0300

    add changes to test.js

--- client_src/js/test.js
+++ client_src/js/test.js
@@ -7,12 +7,10 @@
   Handlebars.registerHelper({
     json: (posts) => {
       return new Handlebars.SafeString(
-        "<pre>" + JSON.stringify(posts, null, 4)  + "</pre>"
+        "<pre>" +  Handlebars.Utils.escapeExpression(JSON.stringify(posts, null, 4))  + "</pre>"
       );
     },
     table: (posts, options) => {
-      console.log(posts);
-
       return posts.map((post, i) => {
         let openTag = "<div class=\"post-stripped\">";
         if (i%2) openTag = "<div>";
