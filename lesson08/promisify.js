fb7abe869ecb7d7de576852c6e92f35fcb5fa4f7
Anna Garmashevska <anniegemail@gmail.com>
2016-08-31 13:45:02 +0300

    fix promisify

--- lesson08/promisify.js
+++ lesson08/promisify.js
@@ -1,7 +1,8 @@
 module.exports = function promisify(asyncFunction) {
   "use strict";
   return function () {
-    var args = [].slice.call(arguments, 0);
+    "use strict";
+    let args = [].slice.call(arguments, 0);
     return new Promise((resolve, reject) => {
       args.push((err, result) => {
         if (err) {
